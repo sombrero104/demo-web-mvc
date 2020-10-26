4facecc0bd7f4b76f32ba0fbfee0b733dc625129
sombrero104 <sombrero.khy@gmail.com>
2020-10-26 23:58:49 +0900

    .

--- src/main/java/me/sombrero/demowebmvc/SampleController.java
+++ src/main/java/me/sombrero/demowebmvc/SampleController.java
@@ -310,7 +310,7 @@ public class SampleController {
      * 핸들러 파라미터에 @MatrixVariable 추가.
      *
      * '/events/1;name=sombrero104'과 같이 요청할 수 있는 방법.
-     * 사용하려면 WebConfig에 configurePathMatch를 설정해줘야 함..
+     * 사용하려면 uri에서 세미콜론(;)을 없애지 않도록 WebConfig에 configurePathMatch를 설정해줘야 함..
      */
     @GetMapping("/events/{id}")
     @ResponseBody
