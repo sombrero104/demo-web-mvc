f8deb8a649474475ff211328fd4f5c83ec839a07
sombrero104 <sombrero.khy@gmail.com>
2020-10-28 15:26:41 +0900

    .

--- src/test/java/me/sombrero/demowebmvc/FileControllerTest.java
+++ src/test/java/me/sombrero/demowebmvc/FileControllerTest.java
@@ -11,6 +11,8 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest // 스프링부트의 전반적인 빈을 사용할 수 있음. 하지만 @WebMvcTest처럼 @AutoConfigureMockMvc을 자동으로 등록해주지는 않음.
@@ -22,12 +24,16 @@ public class FileControllerTest {
 
     @Test
     public void fileUploadTest() throws Exception {
+        // 가짜 파일 만들기.
         // new MockMultipartFile([name: form에서 사용한 이름], [OriginalFilename: 실제 업로드한 파일 이름], [Content-Type], [파일에 들어갈 본문]);
-        MockMultipartFile file = new MockMultipartFile("file", "test.txt", "text/plain", "hello file".getBytes());
+        MockMultipartFile file = new MockMultipartFile(
+                "file", "test.txt", "text/plain", "hello file".getBytes());
 
         // multipart는 POST 요청이고, enctype이 form-data(multipart/form-data)이다.
         // 아래 multipart() 함수가 이런 설정들이 되어 있는 함수라고 생각하면 된다.
-        this.mockMvc.perform(multipart("/file"))
+        this.mockMvc.perform(multipart("/file").file(file))
+            .andDo(print())
+            .andExpect(status().is3xxRedirection());
     }
 
 }
