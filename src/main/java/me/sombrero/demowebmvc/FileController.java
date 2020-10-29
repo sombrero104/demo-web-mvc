e333fdc4578d11758132e5f2fbd7dcc5a4844dd4
sombrero104 <sombrero.khy@gmail.com>
2020-10-29 10:55:05 +0900

    .

--- src/main/java/me/sombrero/demowebmvc/FileController.java
+++ src/main/java/me/sombrero/demowebmvc/FileController.java
@@ -1,15 +1,28 @@
 package me.sombrero.demowebmvc;
 
+import org.apache.catalina.loader.ResourceEntry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.File;
+import java.io.IOException;
+
 @Controller
 public class FileController {
+
+    @Autowired
+    private ResourceLoader resourceLoader;
 
     /**
      * 파일 업로드 페이지.
@@ -34,6 +47,21 @@ public class FileController {
         String message = file.getOriginalFilename() + " is uploaded."; // 업로드 되었다는 메세지.
         attributes.addFlashAttribute("message", message); // 메세지를 RedirectAttributes로 플래시 애트리뷰트에 추가한다.
         return "redirect:/file"; // GET요청의 /file 페이지로 리다이렉트.
+    }
+
+    /**
+     * 파일 다운로드
+     */
+    @GetMapping("/file/{filename}")
+    public ResponseEntity<Resource> fileDownload(@PathVariable String filename) throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:" + filename);
+        File file = resource.getFile();
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        "attachement; filename=\"" + resource.getFilename() + "\"")
+                .header(HttpHeaders.CONTENT_TYPE, "image/png")
+                .header(HttpHeaders.CONTENT_LENGTH, file.length() + "")
+                .body(resource);
     }
 
 }
